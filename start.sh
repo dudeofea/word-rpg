1c0db8b90dbfb70d50bc841b27713b3320debed3
Denis Lachance <dudeofea@hotmail.com>
2016-10-21 21:17:35 -0700

    switched to pure npm script

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-node_modules/watchify/bin/cmd.js rpg.js -o bundle.js &
-node_modules/node-sass/bin/node-sass style.scss -wo . --source-map . &
-node_modules/live-server/live-server.js . --ignore="style.scss,style.css.map"
